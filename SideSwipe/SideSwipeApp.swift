584d12a2434c5d8825767b842d9805aa77a5cc88
oscar <67429870+TheOnlySeru@users.noreply.github.com>
2023-08-14 09:16:02 +0100

    Initial commit

---
+++ SideSwipe/SideSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideSwipeApp.swift
+//  SideSwipe
+//
+//  Created by oscar may on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SideSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SideSwipeUITests/SideSwipeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SideSwipeUITestsLaunchTests.swift
+//  SideSwipeUITests
+//
+//  Created by oscar may on 14/08/2023.
+//
+
+import XCTest
+
+final class SideSwipeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
